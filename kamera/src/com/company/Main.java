8c7f8c015327f06c24c9ba6480b070784aeee139
MateuszHandke2729 <61157533+MateuszHandke2729@users.noreply.github.com>
2020-05-31 23:03:55 +0200

    Update Main.java
    Mamy obszar pomiarowy który jest prostokątem, a jego wymiary to 500x 600 mm. Mamy również kamerę, która robi zdjęcia. Zdjęcia z kamery są kwadratowe i mają rozmiar 40 na 40 mm. Musimy dokonać przejazdu kamerą w taki sposób, aby zebrała ona zdjęcia z całej powierzchni obszaru pomiarowego. 
    Program powinien zwrócić ilość zdjęć wykonanych przez kamerę.

--- kamera/src/com/company/Main.java
+++ kamera/src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+/*Mamy obszar pomiarowy który jest prostokątem, a jego wymiary to 500x 600 mm. Mamy również kamerę, która robi zdjęcia. 
+Zdjęcia z kamery są kwadratowe i mają rozmiar 40 na 40 mm. Musimy dokonać przejazdu kamerą w taki sposób,
+aby zebrała ona zdjęcia z całej powierzchni obszaru pomiarowego. 
+Program powinien zwrócić ilość zdjęć wykonanych przez kamerę. */
+
 public class Main {
     public static class Prostokat
     {
